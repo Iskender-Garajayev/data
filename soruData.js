0faf4c34e907eac9ae9c1dbf528c7468f851e516
Iskender Garajayev <yoloten.isa@gmail.com>
2024-04-02 11:11:52 -0700

    1

---
+++ soruData.js
@@ -0,0 +1,25 @@
+const soruData = [
+  {
+    id: 1,
+    num: 1,
+    soru: "If you want to import just the Component from the React library, what syntax do you use?",
+    a: "A. import React.Component from 'react'" ,
+    b:"B. import [ Component ] from 'react'",
+    c:"C. import React.Component from 'react'",
+    c:"D. import { Component } from 'react'",
+
+    cevap:"Answer: D"
+  },
+  {
+    id: 2,
+    num: 2,
+    soru: "Given the following code, what does this React element look like?",
+    soruOrnek:`React.createElement('h1', null, "What's happening?")`  ,
+    a: "A. <h1 props={null}>What's happening?</h1>" ,
+    b:"B. <h1>What's happening?</h1>",
+    c:`C. <h1 id="component">What's happening?</h1>`,
+    c:`D. <h1 id="element">What's happening?</h1>`,
+
+    cevap:"Answer: D"
+  },
+]
